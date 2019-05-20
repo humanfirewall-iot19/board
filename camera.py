e2de7ae140fd669dc9904da6b1de63be96debfd5
Mattia Nicolella <mattianicolella@gmail.com>
2019-05-20 22:48:36 +0200

    fixed errors in uploading and added preview for debug purposes

--- camera.py
+++ camera.py
@@ -29,14 +29,15 @@ camera.rotation=180
 serial=sha256(getserial().encode("utf-8")).hexdigest()
 
 #we stop the script if the response code is not 200 (ok)
-req_ok=True
-while req_ok:
+while True:
     # we wait for the button to be pressed avoiding polling
     button.wait_for_press()
+    #camera.start_preview()
+    #sleep(5)
     camera.capture('/home/pi/{}.jpg'.format(serial))
+    #camera.stop_preview()
     print("Pressed")
     #sending data to server
-    response= requests.post('http://134.209.95.86:8080/uploader?id='.format(serial),files={'file':open('{}.jpg'.format(serial),'rb')})
-    req_ok= response.status_code == 200
+    response= requests.post('http://134.209.95.86:8080/uploader?id={}'.format(serial),files={'file':open('{}.jpg'.format(serial),'rb')})
+    response.raise_for_status()
     sleep(5)
-response.raise_for_status()
